3dda7384390b40feb024f30c5576ab7ec23941cf
Ronit Joseph <ronitviju@gmail.com>
2023-08-21 05:20:11 +0530

    added pipe

--- README.md
+++ README.md
@@ -9,15 +9,3 @@ During these two weeks, I will focus on getting familiar with Pygame and graspin
 
 Week 3-4: Multiplayer Functionality
 In the following two weeks, I will dive into researching WebSocket communication and selecting a suitable Python library for achieving multiplayer functionality. Libraries like Flask-SocketIO or Django Channels seem promising options. My aim will be to establish a basic server-client architecture that allows multiple players to participate in the game simultaneously. I will focus on implementing synchronized gameplay, enabling players to see and interact with each other's birds.
-
-Week 5-6: Web Implementation
-In these two weeks, I will dedicate my time to learning web development technologies such as HTML, CSS, and JavaScript. Additionally, I will choose a web framework like Flask or Django. With this knowledge, I will create a basic web page that will serve as the game interface. To enable running the game in a web browser, I will integrate my existing Python game code with the chosen web framework.
-
-Week 7-8: WebSocket Integration
-During this period, my focus will be on implementing WebSocket communication between the web client and the server. I will work on establishing real-time communication channels that will enable features like real-time scoring and displaying multiple players' birds. Ensuring smooth and seamless multiplayer interaction will be a priority.
-
-Week 9-10: Refinement and Testing
-These two weeks will be dedicated to thorough testing and debugging of the game. I will conduct comprehensive tests to identify and address any issues or bugs that may arise. Furthermore, I will refine the gameplay experience by improving graphics, user interface, and overall user experience. Testing with multiple players will be crucial to ensuring that the multiplayer functionality functions as intended.
-
-Week 11-12: Deployment and Finalization
-In the final two weeks of the project, my focus will shift towards deployment and finalization. I will select a suitable hosting provider such as Heroku or AWS, and deploy the web application to make it accessible to others. Additionally, I will make any necessary adjustments or optimizations required for the deployed version of the game. Conducting thorough testing on the live server will be essential to ensure that the game performs smoothly and without any issues.
--- main.py
+++ main.py
@@ -27,7 +27,8 @@ pygame.display.set_caption("Flabby bird")
 
 ground_scroll = 0
 scroll_speed = 4
-
+flying = False
+game_over = False
 
 running = True
 bg = pygame.image.load('img/bg.png')
@@ -45,24 +46,54 @@ class Bird(pygame.sprite.Sprite):
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
-    
+        self.vel = 0
+        self.clicked = False
+    #the update func controles the animation aand movement
     def update(self):
-        self.counter+=1
-        flap_cooldown = 5
-        
-        if self.counter >flap_cooldown:
-            self.counter = 0
-            self.index += 1
-            if(self.index >= len(self.images)):
-                self.index = 0
-        self.image = self.images[self.index]
-        
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8                    #basically adding gravity
+            if self.rect.bottom <768:
+                self.rect.y += int(self.vel)     #adding falling capabilty for bird
+        if game_over == False:   
+            #adding jumping abilty for bird
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:    #checks for left click of mouse therfore index [0]
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:                     #checks if mouse is not clicked,or its released 
+                self.clicked = False
+            self.counter+=1
+            flap_cooldown = 5
+            
+            if self.counter >flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if(self.index >= len(self.images)):
+                    self.index = 0
+            self.image = self.images[self.index]
+            
+            #rotate bird
+            self.image = pygame.transform.rotate(self.images[self.index],self.vel * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index],-90)
+            
+class pipe(pygame.sprite.Sprite):
+    def __init__(self,x,y,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x,y]
         
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 
 flappy = Bird(100,int(SCREEN_HEIGHT/2))
 
 bird_group.add(flappy)
+
+btm_pipe = pipe(300,int(SCREEN_HEIGHT/2))
+pipe_group.add(btm_pipe)
 
 
 while running:
@@ -75,11 +106,23 @@ while running:
     bird_group.draw(screen)
     #adding animation to the bird,basically going through various indexes of diff images 
     bird_group.update()
+    
+    pipe_group.draw(screen)
+    pipe_group.update()
+    
+    
     #dmoving teh ground
     screen.blit(ground_img,(ground_scroll,768))
-    ground_scroll -= 4
-    if(abs(ground_scroll) >35):
-        ground_scroll = 0
+    
+    #check if bird touched ground ,as to end the game
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+        
+    if game_over == False:
+        ground_scroll -= scroll_speed
+        if(abs(ground_scroll) >35):
+            ground_scroll = 0
         
     #look at every event in the queue
     for event in pygame.event.get():
@@ -88,7 +131,8 @@ while running:
             #check if pressed key is the escpae key
             if(event.key == K_ESCAPE):
                 running = False
-        
+        if(event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False):
+            flying = True
         elif(event.type == QUIT):
                 running = False
           
